d71cad2085fa40d3c82665e60bdbf9787934af60
yushijinhun <yushijinhun@gmail.com>
2018-02-13 11:54:07 +0800

    remove local config & tweaker

--- src/main/java/org/to2mbn/authlibinjector/AuthlibInjector.java
+++ src/main/java/org/to2mbn/authlibinjector/AuthlibInjector.java
@@ -3,19 +3,11 @@ package org.to2mbn.authlibinjector;
 import static java.util.Optional.empty;
 import static java.util.Optional.of;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.lang.instrument.ClassFileTransformer;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.text.MessageFormat;
 import java.util.Optional;
 import java.util.function.Consumer;
 import org.to2mbn.authlibinjector.transform.ClassTransformer;
-import org.yaml.snakeyaml.Yaml;
 
 public final class AuthlibInjector {
 
@@ -58,62 +50,6 @@ public final class AuthlibInjector {
 	}
 
 	private static Optional<InjectorConfig> configure() {
-		// 1. remote config
-		Optional<InjectorConfig> localConfig = loadLocalConfig();
-		if (localConfig.isPresent()) return localConfig;
-
-		// 2. local config
-		Optional<InjectorConfig> remoteConfig = loadRemoteConfig();
-		if (remoteConfig.isPresent()) return remoteConfig;
-
-		return empty();
-	}
-
-	private static Optional<InjectorConfig> loadLocalConfig() {
-		try {
-			Optional<InputStream> localConfig = locateLocalConfig();
-			if (localConfig.isPresent()) {
-				try (Reader reader = new InputStreamReader(localConfig.get(), StandardCharsets.UTF_8)) {
-					return of(new Yaml().loadAs(reader, InjectorConfig.class));
-				}
-			}
-		} catch (IOException e) {
-			log("unable to configure locally: {0}", e);
-		}
-		return empty();
-	}
-
-	private static Optional<InputStream> locateLocalConfig() throws IOException {
-		// 1. the specified config file
-		String configProperty = System.getProperty("org.to2mbn.authlibinjector.config");
-		if (configProperty != null && !configProperty.startsWith("@")) {
-			Path configFile = Paths.get(configProperty);
-			if (Files.exists(configFile)) {
-				log("using config: " + configProperty);
-				return of(Files.newInputStream(configFile));
-			} else {
-				log("file not exists: {0}", configProperty);
-			}
-		}
-
-		// 2. the config file in jar
-		InputStream packedConfig = AuthlibInjector.class.getResourceAsStream("/authlib-injector.yaml");
-		if (packedConfig != null) {
-			log("using config: jar:/authlib-injector.yaml");
-			return of(packedConfig);
-		}
-
-		// 3. the config in the current dir
-		Path currentConfigFile = Paths.get("authlib-injector.yaml");
-		if (Files.exists(currentConfigFile)) {
-			log("using config: ./authlib-injector.yaml");
-			return of(Files.newInputStream(currentConfigFile));
-		}
-
-		return empty();
-	}
-
-	private static Optional<InjectorConfig> loadRemoteConfig() {
 		String configProperty = System.getProperty("org.to2mbn.authlibinjector.config");
 		if (configProperty == null || !configProperty.startsWith("@")) {
 			return empty();
