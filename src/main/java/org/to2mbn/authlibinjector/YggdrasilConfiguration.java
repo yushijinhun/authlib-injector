da81ef6f60437014aa561a9acd7a5856b9e69acb
yushijinhun <yushijinhun@gmail.com>
2018-04-15 09:57:55 +0800

    更换到json-simple类库

--- src/main/java/org/to2mbn/authlibinjector/YggdrasilConfiguration.java
+++ src/main/java/org/to2mbn/authlibinjector/YggdrasilConfiguration.java
@@ -1,69 +1,59 @@
 package org.to2mbn.authlibinjector;
 
 import static java.text.MessageFormat.format;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.emptyMap;
 import static java.util.Collections.unmodifiableList;
 import static java.util.Collections.unmodifiableMap;
 import static java.util.Objects.requireNonNull;
-import static java.util.Optional.empty;
-import static java.util.Optional.of;
 import static java.util.Optional.ofNullable;
-import static org.to2mbn.authlibinjector.util.KeyUtils.decodePublicKey;
-import static org.to2mbn.authlibinjector.util.KeyUtils.loadX509PublicKey;
-import java.io.IOException;
-import java.security.GeneralSecurityException;
+import static java.util.stream.Collectors.toList;
+import static org.to2mbn.authlibinjector.util.JsonUtils.asArray;
+import static org.to2mbn.authlibinjector.util.JsonUtils.asObject;
+import static org.to2mbn.authlibinjector.util.JsonUtils.parseJson;
+import java.io.UncheckedIOException;
 import java.security.PublicKey;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.TreeMap;
-import org.to2mbn.authlibinjector.internal.org.json.JSONException;
-import org.to2mbn.authlibinjector.internal.org.json.JSONObject;
+import org.to2mbn.authlibinjector.internal.org.json.simple.JSONObject;
+import org.to2mbn.authlibinjector.util.JsonUtils;
+import org.to2mbn.authlibinjector.util.KeyUtils;
 
 public class YggdrasilConfiguration {
 
-	public static YggdrasilConfiguration parse(String apiRoot, String metadataResponse) throws IOException {
+	public static YggdrasilConfiguration parse(String apiRoot, String metadataResponse) throws UncheckedIOException {
 		if (!apiRoot.endsWith("/")) apiRoot += "/";
 
-		try {
-			JSONObject response = new JSONObject(metadataResponse);
+		JSONObject response = asObject(parseJson(metadataResponse));
 
-			List<String> skinDomains = new ArrayList<>();
-			ofNullable(response.optJSONArray("skinDomains"))
-					.ifPresent(it -> it.forEach(domain -> {
-						if (domain instanceof String)
-							skinDomains.add((String) domain);
-					}));
+		List<String> skinDomains =
+				ofNullable(response.get("skinDomains"))
+						.map(it -> asArray(it).stream()
+								.map(JsonUtils::asString)
+								.collect(toList()))
+						.orElse(emptyList());
 
-			Optional<PublicKey> decodedPublickey;
-			String publickeyString = response.optString("signaturePublickey");
-			if (publickeyString == null) {
-				decodedPublickey = empty();
-			} else {
-				try {
-					decodedPublickey = of(loadX509PublicKey(decodePublicKey(publickeyString)));
-				} catch (IllegalArgumentException | GeneralSecurityException e) {
-					throw new IOException("Bad signature publickey", e);
-				}
-			}
+		Optional<PublicKey> decodedPublickey =
+				ofNullable(response.get("signaturePublickey"))
+						.map(JsonUtils::asString)
+						.map(KeyUtils::parseSignaturePublicKey);
 
-			Map<String, String> meta = new TreeMap<>();
-			ofNullable(response.optJSONObject("meta"))
-					.map(JSONObject::toMap)
-					.ifPresent(it -> it.forEach((k, v) -> meta.put(k, String.valueOf(v))));
+		Map<String, Object> meta =
+				ofNullable(response.get("meta"))
+						.map(it -> (Map<String, Object>) new TreeMap<>(asObject(it)))
+						.orElse(emptyMap());
 
-			return new YggdrasilConfiguration(apiRoot, unmodifiableList(skinDomains), unmodifiableMap(meta), decodedPublickey);
-		} catch (JSONException e) {
-			throw new IOException("Invalid json", e);
-		}
+		return new YggdrasilConfiguration(apiRoot, unmodifiableList(skinDomains), unmodifiableMap(meta), decodedPublickey);
 	}
 
 	private String apiRoot;
 	private List<String> skinDomains;
 	private Optional<PublicKey> decodedPublickey;
-	private Map<String, String> meta;
+	private Map<String, Object> meta;
 
-	public YggdrasilConfiguration(String apiRoot, List<String> skinDomains, Map<String, String> meta, Optional<PublicKey> decodedPublickey) {
+	public YggdrasilConfiguration(String apiRoot, List<String> skinDomains, Map<String, Object> meta, Optional<PublicKey> decodedPublickey) {
 		this.apiRoot = requireNonNull(apiRoot);
 		this.skinDomains = requireNonNull(skinDomains);
 		this.meta = requireNonNull(meta);
@@ -78,7 +68,7 @@ public class YggdrasilConfiguration {
 		return skinDomains;
 	}
 
-	public Map<String, String> getMeta() {
+	public Map<String, Object> getMeta() {
 		return meta;
 	}
 
--- src/test/java/org/to2mbn/authlibinjector/test/KeyUtilsTest.java
+++ src/test/java/org/to2mbn/authlibinjector/test/KeyUtilsTest.java
@@ -1,7 +1,7 @@
 package org.to2mbn.authlibinjector.test;
 
 import static org.junit.Assert.assertArrayEquals;
-import static org.to2mbn.authlibinjector.util.KeyUtils.decodePublicKey;
+import static org.to2mbn.authlibinjector.util.KeyUtils.decodePEMPublicKey;
 import org.junit.Test;
 
 public class KeyUtilsTest {
@@ -9,23 +9,23 @@ public class KeyUtilsTest {
 	@Test
 	public void testDecodePublicKey1() {
 		assertArrayEquals(new byte[] { 127, 127, 127, 127 },
-				decodePublicKey("-----BEGIN PUBLIC KEY-----f39/fw==-----END PUBLIC KEY-----"));
+				decodePEMPublicKey("-----BEGIN PUBLIC KEY-----f39/fw==-----END PUBLIC KEY-----"));
 	}
 
 	@Test
 	public void testDecodePublicKey2() {
 		assertArrayEquals(new byte[] { 127, 127, 127, 127 },
-				decodePublicKey("-----BEGIN PUBLIC KEY-----\nf\n39/fw==\n-----END PUBLIC KEY-----\n"));
+				decodePEMPublicKey("-----BEGIN PUBLIC KEY-----\nf\n39/fw==\n-----END PUBLIC KEY-----\n"));
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testDecodePublicKey3() {
-		decodePublicKey("-----BEGIN PUBLIC KEY----- f39/fw== -----END PUBLIC KEY-----");
+		decodePEMPublicKey("-----BEGIN PUBLIC KEY----- f39/fw== -----END PUBLIC KEY-----");
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testDecodePublicKey4() {
-		decodePublicKey("-----BEGIN PUBLIC KEY-----f39/fw==-----END NOT A PUBLIC KEY-----");
+		decodePEMPublicKey("-----BEGIN PUBLIC KEY-----f39/fw==-----END NOT A PUBLIC KEY-----");
 	}
 
 }
