106e13b17f0a5516cbff94cf88b0b0ab1065e8b8
yushijinhun <yushijinhun@gmail.com>
2020-04-29 23:34:12 +0800

    Revert "Add classloader exclusion to launchwrapper (#68)"
    This reverts commit 2ac95074a6ce21133ec5854c2e8cff84cb974f91.

--- src/main/java/moe/yushi/authlibinjector/AuthlibInjector.java
+++ src/main/java/moe/yushi/authlibinjector/AuthlibInjector.java
@@ -60,7 +60,6 @@ import moe.yushi.authlibinjector.transform.DumpClassListener;
 import moe.yushi.authlibinjector.transform.support.AuthlibLogInterceptor;
 import moe.yushi.authlibinjector.transform.support.CitizensTransformer;
 import moe.yushi.authlibinjector.transform.support.ConstantURLTransformUnit;
-import moe.yushi.authlibinjector.transform.support.LaunchwrapperExclusionTransformer;
 import moe.yushi.authlibinjector.transform.support.MC52974Workaround;
 import moe.yushi.authlibinjector.transform.support.MC52974_1710Workaround;
 import moe.yushi.authlibinjector.transform.support.MainArgumentsTransformer;
@@ -450,7 +449,6 @@ public final class AuthlibInjector {
 			transformer.units.add(new AuthlibLogInterceptor());
 		}
 
-		transformer.units.add(new LaunchwrapperExclusionTransformer());
 		transformer.units.add(new MainArgumentsTransformer());
 		transformer.units.add(new ConstantURLTransformUnit(urlProcessor));
 		transformer.units.add(new CitizensTransformer());
