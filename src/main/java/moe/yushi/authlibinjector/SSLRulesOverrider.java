0b5909601bde67aaccba95369e14ddaee913c155
Shark-vil <shark_vil@mail.ru>
2021-05-17 05:19:55 +0300

    Added a class for changing the rules SSL checking

---
+++ src/main/java/moe/yushi/authlibinjector/SSLRulesOverrider.java
@@ -0,0 +1,39 @@
+package moe.yushi.authlibinjector;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.GeneralSecurityException;
+import java.security.cert.X509Certificate;
+
+import static moe.yushi.authlibinjector.util.Logging.Level.ERROR;
+import static moe.yushi.authlibinjector.util.Logging.log;
+
+public class SSLRulesOverrider {
+    public static void TrustUnknownCertificates() {
+        // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[] {
+            new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }
+                public void checkClientTrusted(
+                    java.security.cert.X509Certificate[] certs, String authType) {
+                }
+                public void checkServerTrusted(
+                    java.security.cert.X509Certificate[] certs, String authType) {
+                }
+            }
+        };
+
+        try {
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (GeneralSecurityException e) {
+            log(ERROR, "Failed to override the SSL rules: " + e);
+            throw new InitializationException(e);
+        }
+    }
+}
