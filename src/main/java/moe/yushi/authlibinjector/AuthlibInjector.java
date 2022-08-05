9c0f278bb41d4b1dfcb598428163093fa06516b3
Haowei Wen <yushijinhun@gmail.com>
2022-08-06 00:19:07 +0800

    Support Velocity MC 1.19+

--- src/main/java/moe/yushi/authlibinjector/AuthlibInjector.java
+++ src/main/java/moe/yushi/authlibinjector/AuthlibInjector.java
@@ -65,6 +65,7 @@ import moe.yushi.authlibinjector.transform.support.PaperUsernameCheckTransformer
 import moe.yushi.authlibinjector.transform.support.ProxyParameterWorkaround;
 import moe.yushi.authlibinjector.transform.support.SkinWhitelistTransformUnit;
 import moe.yushi.authlibinjector.transform.support.UsernameCharacterCheckTransformer;
+import moe.yushi.authlibinjector.transform.support.VelocityIdentifiedKeyImplTransformUnit;
 import moe.yushi.authlibinjector.transform.support.YggdrasilKeyTransformUnit;
 import moe.yushi.authlibinjector.yggdrasil.CustomYggdrasilAPIProvider;
 import moe.yushi.authlibinjector.yggdrasil.MojangYggdrasilAPIProvider;
@@ -291,6 +292,7 @@ public final class AuthlibInjector {
 
 		transformer.units.add(new YggdrasilKeyTransformUnit());
 		config.getDecodedPublickey().ifPresent(YggdrasilKeyTransformUnit.PUBLIC_KEYS::add);
+		transformer.units.add(new VelocityIdentifiedKeyImplTransformUnit());
 
 		return transformer;
 	}
