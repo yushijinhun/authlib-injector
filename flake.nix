7bcd48dffb25535d97070e9b1d276aae3759a9cc
Evan Goode <mail@evangoo.de>
2026-04-25 21:05:16 -0400

    Add flake.nix with a devShell

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "authlib-injector development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            jdk25
+            gradle
+            mdbook
+          ];
+
+          JAVA_HOME = "${pkgs.jdk25}";
+        };
+      }
+    );
+}
